58fabe2182ea402b8ab309ac85230e3fa0687159
charlesyu1203 <43996689+charlesyu1203@users.noreply.github.com>
2019-03-17 22:38:47 -0400

    Fun
    Level 1

---
+++ Platformer/Assets/Scripts/GameControl.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameControl : MonoBehaviour
+{
+    public static GameControl instance;
+    public Text scoreText;
+    private int score = 0;
+
+
+    void Awake()
+    {
+        if (instance == null)
+            //...set this one to be it...
+            instance = this;
+        //...otherwise...
+        else if (instance != this)
+            //...destroy this one because it is a duplicate.
+            Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        //If the game is over and the player has pressed some input...
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        if (Input.GetKeyDown(KeyCode.Escape) == true)
+        {
+            Application.Quit();
+            Debug.Log("Exit Game.");
+        }
+    }
+
+    public void KnightScored()
+    { 
+        //If the game is not over, increase the score...
+        score++;
+        //...and adjust the score text.
+        scoreText.text = "Score: " + score.ToString();
+    }
+}
