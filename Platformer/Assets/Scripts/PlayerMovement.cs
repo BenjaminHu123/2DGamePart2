d78987d703ef396a3fc896caff802e4ec2bed130
BenjaminHu123 <43998171+BenjaminHu123@users.noreply.github.com>
2019-03-18 11:08:11 -0400

    scene movement done

--- Platformer/Assets/Scripts/PlayerMovement.cs
+++ Platformer/Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour {
 
@@ -136,7 +137,7 @@ public class PlayerMovement : MonoBehaviour {
         }
         if (collision.gameObject.CompareTag("winning"))
         {
-            Debug.Log("ewr");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 
